acbd795f56e65194d99f97fd894e9484193b60b8
KTN__ <kritagyadahal588@gmail.com>
2023-10-03 15:56:45 +0545

    Reaction Time Work Completed (Aced)

---
+++ Reaction Time/main.py
@@ -0,0 +1,16 @@
+import pyautogui
+
+# FAILSAFE just in case
+pyautogui.FAILSAFE = True
+
+# Defining the ONLY clickable color (slight green)
+clickableColor = (75, 219, 106)
+
+# Infinite loop to check the mouse's position and the color at the position
+while True:
+    x, y = pyautogui.position()
+    hoveredColor = pyautogui.pixel(x, y)
+
+    # Check if mouse is hovering over the clickable color
+    if hoveredColor == clickableColor:
+        pyautogui.doubleClick()
