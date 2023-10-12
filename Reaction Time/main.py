0fde114586b661d3d3f961ddeb7647de10e25192
KTN__ <kritagyadahal588@gmail.com>
2023-10-12 07:25:55 +0545

    Optimized reaction time bot

--- Reaction Time/main.py
+++ Reaction Time/main.py
@@ -1,16 +1,30 @@
 import pyautogui
+import keyboard
+import win32api, win32con
+import time
 
 # FAILSAFE, just in case
 pyautogui.FAILSAFE = True
+
+
+# win32 click function(faster than pyautogui.click/doubleclick)
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.01)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
 
 # Defining the ONLY clickable color (slight green)
 clickableColor = (75, 219, 106)
 
 # Infinite loop to check the mouse's position and the color at the position
-while True:
+while keyboard.is_pressed("q") == False:
     x, y = pyautogui.position()
     hoveredColor = pyautogui.pixel(x, y)
 
     # Check if mouse is hovering over the clickable color
     if hoveredColor == clickableColor:
-        pyautogui.doubleClick()
+        # Looping left the chat
+        click(x, y)
+        click(x, y)
